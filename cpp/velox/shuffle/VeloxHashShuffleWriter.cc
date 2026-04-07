6d5f57f855960fb1b927b10c575691b36ff4f88c
Wangyang Guo <wangyang.guo@intel.com>
2026-04-07 11:41:07 +0800

    TypeAwareCompress(tac) for column-wise data compression like (U)INT64 in shuffle

--- cpp/velox/shuffle/VeloxHashShuffleWriter.cc
+++ cpp/velox/shuffle/VeloxHashShuffleWriter.cc
@@ -17,6 +17,7 @@
 
 #include "shuffle/VeloxHashShuffleWriter.h"
 #include "memory/ArrowMemory.h"
+#include "shuffle/VeloxTypeAwareCompress.h"
 #include "memory/VeloxColumnarBatch.h"
 #include "shuffle/Utils.h"
 #include "utils/Common.h"
@@ -764,6 +765,7 @@ arrow::Status VeloxHashShuffleWriter::initColumnTypes(const facebook::velox::Row
         isValidityBuffer_.push_back(true);
         isValidityBuffer_.push_back(false);
         isValidityBuffer_.push_back(false);
+        bufferTypes_.insert(bufferTypes_.end(), 3, tac::kUnsupported);
       } break;
       case arrow::StructType::type_id:
       case arrow::MapType::type_id:
@@ -777,6 +779,7 @@ arrow::Status VeloxHashShuffleWriter::initColumnTypes(const facebook::velox::Row
         simpleColumnIndices_.push_back(i);
         isValidityBuffer_.push_back(true);
         isValidityBuffer_.push_back(boolIsBit());
+        bufferTypes_.insert(bufferTypes_.end(), 2, tac::kUnsupported);
       } break;
       case arrow::NullType::type_id:
         break;
@@ -784,12 +787,15 @@ arrow::Status VeloxHashShuffleWriter::initColumnTypes(const facebook::velox::Row
         simpleColumnIndices_.push_back(i);
         isValidityBuffer_.push_back(true);
         isValidityBuffer_.push_back(false);
+        bufferTypes_.push_back(tac::kUnsupported); // validity
+        bufferTypes_.push_back(veloxTypeToTacType(veloxColumnTypes_[i]->kind())); // data
       } break;
     }
   }
 
   if (hasComplexType_) {
     isValidityBuffer_.push_back(false);
+    bufferTypes_.push_back(tac::kUnsupported);
   }
 
   fixedWidthColumnCount_ = simpleColumnIndices_.size();
@@ -973,8 +979,9 @@ arrow::Status VeloxHashShuffleWriter::evictBuffers(
     std::vector<std::shared_ptr<arrow::Buffer>> buffers,
     bool reuseBuffers) {
   if (!buffers.empty()) {
-    auto payload =
-        std::make_unique<InMemoryPayload>(numRows, &isValidityBuffer_, schema_, std::move(buffers), hasComplexType_);
+    auto* types = partitionWriter_->enableTypeAwareCompress() ? &bufferTypes_ : nullptr;
+    auto payload = std::make_unique<InMemoryPayload>(
+        numRows, &isValidityBuffer_, schema_, std::move(buffers), hasComplexType_, types);
     RETURN_NOT_OK(partitionWriter_->hashEvict(partitionId, std::move(payload), Evict::kCache, reuseBuffers, writtenBytes_));
   }
   return arrow::Status::OK();
@@ -1393,8 +1400,9 @@ arrow::Result<int64_t> VeloxHashShuffleWriter::evictPartitionBuffersMinSize(int6
     for (auto& item : pidToSize) {
       auto pid = item.first;
       ARROW_ASSIGN_OR_RAISE(auto buffers, assembleBuffers(pid, false));
+      auto* types = partitionWriter_->enableTypeAwareCompress() ? &bufferTypes_ : nullptr;
       auto payload = std::make_unique<InMemoryPayload>(
-          item.second, &isValidityBuffer_, schema_, std::move(buffers), hasComplexType_);
+          item.second, &isValidityBuffer_, schema_, std::move(buffers), hasComplexType_, types);
       metrics_.totalBytesToEvict += payload->rawSize();
       RETURN_NOT_OK(partitionWriter_->hashEvict(pid, std::move(payload), Evict::kSpill, false, writtenBytes_));
       evicted = beforeEvict - partitionBufferPool_->bytes_allocated();
