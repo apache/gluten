0943205b05011497d7e8d94cf3845abd011c9a05
Wangyang Guo <wangyang.guo@intel.com>
2026-04-16 17:34:51 +0800

    Move Apache license to header

--- cpp/core/utils/tac/ffor.hpp
+++ cpp/core/utils/tac/ffor.hpp
@@ -1,4 +1,21 @@
 /*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
  * MIT License
  *
  * Copyright (c) 2024 Azim Afroozeh, CWI Database Architectures Group
@@ -24,16 +41,7 @@
  * ---
  *
  * Modifications Copyright (c) 2026 Wangyang Guo, licensed under the
- * Apache License, Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Apache License, Version 2.0.
  */
 
 // 4-lane FFOR (Frame-of-Reference + Bit-Packing) codec for uint64_t.
