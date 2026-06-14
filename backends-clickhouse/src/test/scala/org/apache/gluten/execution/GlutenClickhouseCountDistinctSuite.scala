a10ba49cc8e4741a512b3b9dfbb7d5decbbd2b54
Navaneeth Sujith <navaneethsujith09@gmail.com>
2026-06-14 11:19:28 -0700

    [CH] Support skewness aggregate function

--- backends-clickhouse/src/test/scala/org/apache/gluten/execution/GlutenClickhouseCountDistinctSuite.scala
+++ backends-clickhouse/src/test/scala/org/apache/gluten/execution/GlutenClickhouseCountDistinctSuite.scala
@@ -103,9 +103,8 @@ class GlutenClickhouseCountDistinctSuite extends GlutenClickHouseWholeStageTrans
     // skewness agg is not supported, will cause fallback
     val sql = "select count(distinct(a,b)) , skewness(b) from " +
       "values (0, null,1), (0,null,1), (1, 1,1), (2, 2, 1) ,(2,2,2),(3,3,3) as data(a,b,c)"
-    assertThrows[UnsupportedOperationException] {
-      spark.sql(sql).show
-    }
+    compareResultsAgainstVanillaSpark(sql, true, { _ => })
+
   }
 
   test("check count distinct with expr fallback") {
