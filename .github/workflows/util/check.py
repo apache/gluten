5ee9fe02531e6cc1d7bec4dcba4a47587ab69217
Yuan <yuanzhou@apache.org>
2026-06-16 18:05:26 +0300

    fix
    Signed-off-by: Yuan <yuanzhou@apache.org>

--- .github/workflows/util/check.py
+++ .github/workflows/util/check.py
@@ -63,11 +63,7 @@ def get_files(commit, path):
             for name in files:
                 filelist.append(os.path.join(root, name))
 
-    return [
-        file
-        for file in filelist
-        if "ep/build-velox" not in file
-    ]
+    return [file for file in filelist if "ep/build-velox" not in file]
 
 
 def help(args):
