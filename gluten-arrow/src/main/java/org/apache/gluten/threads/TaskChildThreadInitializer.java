aff4d87650632040b2e48324ae28870ee3b76567
Hongze Zhang <hongze.zzz123@gmail.com>
2026-06-16 11:17:37 +0200

    style

--- gluten-arrow/src/main/java/org/apache/gluten/threads/TaskChildThreadInitializer.java
+++ gluten-arrow/src/main/java/org/apache/gluten/threads/TaskChildThreadInitializer.java
@@ -39,9 +39,6 @@ public class TaskChildThreadInitializer implements NativeThreadInitializer {
   private final TaskContext parentTaskContext;
   private final Map<String, String> childThreads = new ConcurrentHashMap<>();
 
-  /**
-   * @param parentTaskContext the Spark task's context to propagate to child native threads.
-   */
   public TaskChildThreadInitializer(TaskContext parentTaskContext) {
     Preconditions.checkNotNull(parentTaskContext);
     this.parentTaskContext = parentTaskContext;
