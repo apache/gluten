ebe6df2fc8f57d8849e51e0dfe296f9d6136f56f
Navaneeth Sujith <navaneethsujith09@gmail.com>
2026-06-22 22:46:48 -0700

    added new test logic

--- backends-clickhouse/src/test/scala/org/apache/gluten/execution/GlutenClickhouseCountDistinctSuite.scala
+++ backends-clickhouse/src/test/scala/org/apache/gluten/execution/GlutenClickhouseCountDistinctSuite.scala
@@ -106,6 +106,11 @@ class GlutenClickhouseCountDistinctSuite extends GlutenClickHouseWholeStageTrans
     GlutenQueryComparisonTest.checkFallBack(df, noFallback = true)
     compareResultsAgainstVanillaSpark(sql, true, { _ => })
 
+    val sqlWithKeys = "select a, count(distinct(b)) , skewness(b) from " +
+      "values (0, null,1), (0,null,1), (1, 1,1), (2, 2, 1) ,(2,2,2),(3,3,3) as data(a,b,c) group by a"
+    val dfWithKeys = spark.sql(sqlWithKeys)
+    GlutenQueryComparisonTest.checkFallBack(dfWithKeys, noFallback = true)
+    compareResultsAgainstVanillaSpark(sqlWithKeys, true, { _ => })
   }
 
   test("check count distinct with expr fallback") {
